0eeb2788da1abfc485970fa393e6e1fa707cd810
Gaurav Rathore <gauravrathore856@gmail.com>
2026-03-10 17:15:17 +0530

    Adding loaders

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-﻿// src/app/layout.tsx
 import "./globals.css";
+import NextTopLoader from "nextjs-toploader";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
+        <NextTopLoader color="#000" showSpinner={false} />
         {children}
       </body>
     </html>
